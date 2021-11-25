12ce488a2696eb8c64ad03eabcdb1d47a51a0bb7
Mariana Hrytsiuk <mariana.hrytsiuk@medecision.com>
2021-11-25 23:26:18 +0200

    Refactor project structure, add UI module

--- src/com/company/IElevatorStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-import java.util.LinkedHashSet;
-
-
-public interface IElevatorStrategy {
-    public LinkedHashSet<Integer> buildRoute(IElevator floor);
-}
--- src/com/company/IFloor.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-import java.util.ArrayList;
-
-public interface IFloor {
-    public ArrayList<Passenger> getPassengers();
-}
